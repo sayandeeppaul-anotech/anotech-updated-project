9a262b41c92efe772b27bdf6e518bbe5bee5666f
Rishavgoswami10 <rishav.goswami@anotechsolutions.com>
2024-06-24 13:26:21 -0900

    new update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ setupWebSocket(server);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3002'], 
+  origin: '*', 
   credentials: true
 }));
 
